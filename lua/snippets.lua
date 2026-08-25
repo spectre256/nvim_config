2feef161dfe0fb02a80bff0f46d49e9261f669de
Ellis Gibbons <egibbons256@gmail.com>
2026-08-24 21:01:21 -0400

    Change snippets to use lpeg patterns

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -1,11 +1,15 @@
 local api = vim.api
 local map = vim.keymap.set
+local lpeg = vim.lpeg
+local k = vim.keycode
 
 -- TODO: General purpose "expand snippet with" keymap
 
 -- Smart unexpanding snippets
 local Snippets = {
+    langs = {},
     rules = {},
+    patterns = {},
     state = {
         ns = api.nvim_create_namespace("snippets"),
         expanded = false,
@@ -15,34 +19,77 @@ local Snippets = {
         last_col = 0,
     },
 }
+setmetatable(Snippets, Snippets)
 
-local function toSet(arr)
-    local set = {}
-    for _, v in ipairs(arr) do
-        set[v] = true
+function make_defs(rules)
+    local defs = {}
+    for name, _ in pairs(rules) do
+        defs[name] = lpeg.V(name)
     end
-    return set
+    return defs
+end
+
+function Snippets:lang(lang_name, rules)
+    local defs = make_defs(rules)
+    for rule_name, rule in pairs(rules) do
+        if type(rule) == "string" then
+            rules[rule_name] = vim.re.compile(rule, defs)
+        end
+    end
+    rules.any = rules.any or lpeg.P(1) ^ 0
+
+    self.langs[lang_name] = rules
+end
+
+function Snippets:build(lang)
+    if not self.rules[lang] then return nil end
+
+    -- Create set of alternations per context
+    local branches = {}
+    for _, rule in pairs(self.rules[lang]) do
+        pattern = rule.lhs / rule.rhs
+
+        for _, ctx in ipairs(rule.ctx) do
+            branches[ctx] = branches[ctx] and pattern + branches[ctx] or pattern
+        end
+    end
+
+    -- Combine into single pattern
+    local combined = vim.iter(branches)
+        :fold(-lpeg.P(1), function(acc, ctx, pattern)
+            return acc + lpeg.V(ctx) * pattern
+        end)
+    combined = lpeg.Ct(lpeg.Cg(combined, "match") * lpeg.Cg(lpeg.Cp(), "pos"))
+
+    -- Build grammar
+    local grammar = vim.tbl_extend("error", { "top", top = combined }, self.langs[lang])
+    return lpeg.P(grammar)
+end
+
+function Snippets:pattern(lang)
+    if not self.patterns[lang] then self.patterns[lang] = Snippets:build(lang) end
+    return self.patterns[lang]
 end
 
 function Snippets:add(langs, lhs, rhs, opts)
     if type(langs) ~= "table" then langs = { langs } end
-    opts = opts or {}
+    opts = vim.tbl_extend("keep", opts or {}, { ctx = { "any" } })
 
     for _, lang in ipairs(langs) do
         if not self.rules[lang] then self.rules[lang] = {} end
+        local computed_lhs = type(lhs) == "string" and vim.re.compile(lhs, make_defs(self.langs[lang])) or lhs
 
         table.insert(self.rules[lang], {
             lang = lang,
-            lhs = lhs .. "$",
+            lhs = computed_lhs,
             rhs = rhs,
-            include = opts.include and toSet(opts.include),
-            exclude = toSet(opts.exclude or {}),
+            ctx = opts.ctx,
         })
     end
 end
 
-function Snippets.snip(lang, lhs, rhs, opts)
-    Snippets:add(lang, lhs, rhs, opts)
+function Snippets:__call(langs, lhs, rhs, opts)
+    self:add(langs, lhs, rhs, opts)
 end
 
 function Snippets.state:reset(bufnr)
@@ -79,7 +126,7 @@ function Snippets:unexpand_snippet()
     if self.state:is_saved() then
         vim.snippet.stop()
         self.state:restore(0)
-        api.nvim_feedkeys(vim.keycode("<Esc>a"), "n", false)
+        api.nvim_feedkeys(k"<Esc>a", "n", false)
         return true
     else
         return false
@@ -87,93 +134,58 @@ function Snippets:unexpand_snippet()
 end
 
 function Snippets:on_key(ev)
-    do
-        local ok, parser = pcall(vim.treesitter.get_parser, ev.buf)
-        if not ok or not parser then goto skip end
+    -- 0-indexed row and col
+    local row, col = unpack(api.nvim_win_get_cursor(0))
+    row = row - 1
 
-        local tree = parser:parse()[1]
-        if not tree then goto skip end
+    -- Only expand when adding characters, not removing
+    local added_char = row == self.state.last_row and col > self.state.last_col
+    self.state.last_row, self.state.last_col = row, col
+    if not added_char then return end
 
-        -- 0-indexed row and col
-        local row, col = unpack(api.nvim_win_get_cursor(0))
-        row = row - 1
+    local pattern = self:pattern(vim.bo[ev.buf].filetype)
+    if not pattern then return end
 
-        -- Only expand when adding characters, not removing
-        local added_char = row == self.state.last_row and col > self.state.last_col
-        self.state.last_row, self.state.last_col = row, col
-        if not added_char then goto skip end
-
-        local lang_snippets = self.rules[parser:lang()]
-        if not lang_snippets then goto skip end
-
-        -- Subtract one to get node at beginning of cursor, necessary when typing at end of line
-        local line = api.nvim_get_current_line():sub(1, col)
-
-        for _, s in ipairs(lang_snippets) do
-            local captures = vim.fn.matchlist(line, s.lhs)
-            if #captures == 0 then goto continue end
-
-            local match = captures[1]
-            local i = col - #match
-
-            -- Check node type just before match to avoid polluting check with typed characters
-            local node = tree:root():named_descendant_for_range(row, i - 1, row, i - 1)
-            local node_type = node and node:type()
-            if s.include and not s.include[node_type] then goto continue end
-            if s.exclude[node_type] then goto continue end
-
-            self.state:save(ev.buf, match, row, i, row, col)
-            api.nvim_buf_set_text(ev.buf, row, i, row, col, {})
-
-            local rhs
-            if type(s.rhs) == "function" then
-                rhs = s.rhs(captures)
-            else
-                rhs = s.rhs:gsub("%$(%d+)", function(num_str)
-                    local cap_i = tonumber(num_str)
-                    return captures[cap_i + 1]
-                end)
-            end
-
-            vim.snippet.expand(rhs)
-            self.state.expanded = true
-            -- Return immediately to avoid unsetting expand
-            do return end
-
-            ::continue::
-        end
+    local line = api.nvim_get_current_line()
+    local result = pattern:match(line)
+    if result and result.pos == col + 1 then
+        self.state:save(ev.buf, line:sub(1, col), row, 0, row, col)
+        api.nvim_buf_set_text(ev.buf, row, 0, row, col, {})
+        vim.snippet.expand(result.match)
+        self.state.expanded = true
+    else
+        self.state.expanded = false
     end
-
-    ::skip::
-    self.state.expanded = false
 end
 
 function Snippets:setup()
     map({ "i", "s" }, "<C-e>", function()
         if not self:unexpand_snippet() then
-            api.nvim_feedkeys(vim.keycode("<C-e>"), "n", false)
+            api.nvim_feedkeys(k"<C-e>", "n", false)
         end
     end)
 
     local ok, npairs = pcall(require, "nvim-autopairs")
     map({ "i", "s" }, "<BS>", function()
         if not (self.state.expanded and self:unexpand_snippet()) then
-            api.nvim_feedkeys(ok and npairs.autopairs_bs() or vim.keycode("<BS>"), "n", false)
+            api.nvim_feedkeys(ok and npairs.autopairs_bs() or k"<BS>", "n", false)
             self.state.expanded = false
         end
     end)
+
     map({ "i", "s" }, "<Tab>", function()
         if vim.snippet.active({ direction = 1 }) then
             self.state.expanded = false
             vim.snippet.jump(1)
         else
-            api.nvim_feedkeys(vim.keycode("<Tab>"), "n", false)
+            api.nvim_feedkeys(k"<Tab>", "n", false)
         end
     end)
+
     map({ "i", "s" }, "<Esc>", function()
         if vim.snippet.active() then vim.snippet.stop() end
 
-        api.nvim_feedkeys(vim.keycode("<Esc>"), "n", false)
+        api.nvim_feedkeys(k"<Esc>", "n", false)
     end)
 
     -- Auto-expand snippets
